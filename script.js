eb0c135f700d50204e7472ece54144477ed50471
pokediger293983893 <146837422+pokediger293983893@users.noreply.github.com>
2023-11-05 17:01:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+// script.js
 
+const API_KEY = 'YOUR_OPENWEATHERMAP_API_KEY';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Function to fetch weather data
+async function getWeatherData(city) {
+  try {
+    const response = await fetch(`${baseUrl}?q=${city}&appid=${API_KEY}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+  }
+}
+
+// Function to update the weather information on the webpage
+async function updateWeather(city) {
+  const weatherData = await getWeatherData(city);
+  if (weatherData) {
+    const weatherElement = document.querySelector('.weather');
+    weatherElement.innerHTML = `
+      <h2>Weather Forecast for ${city}</h2>
+      <p>Temperature: ${weatherData.main.temp} K</p>
+      <p>Weather: ${weatherData.weather[0].main}</p>
+    `;
+  }
+}
+
+// Example usage
+const city = 'New York'; // Replace with the desired city
+updateWeather(city);
